04cafb6e255ffce5fbfc57fc9272af682c1a989f
Juntong Ye <juntoyt1@uci.edu>
2024-02-04 21:17:57 -0800

    write strdup

--- src/string.cpp
+++ src/string.cpp
@@ -6,3 +6,20 @@ int String::strlen(const char *s){
     }
     return length;
 }
+
+char* String::strcpy(char* dest, const char* src) {
+     int i = 0;
+      for ( i = 0; src[i] != '\0'; ++i)  
+      {
+	    dest[i] = src[i];
+      }
+      dest[i]  = '\0';
+      return dest;
+}
+
+char* String::strdup(const char *src){
+    int length = strlen(src);
+    char* new_str = new char[length + 1]; // +1 for null terminator
+    strcpy(new_str, src);
+    return new_str;
+}
