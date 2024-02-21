16183e6af3ff6ec1c3f85acecd02056f78761b89
Juntong Ye <juntoyt1@uci.edu>
2024-02-20 19:13:16 -0800

    finished shape.cpp and working on picture

--- src/picture.cpp
+++ src/picture.cpp
@@ -1,3 +1,29 @@
 #include "picture.hpp"
 #include <iostream>
 #include <utility>
+#include <algorithm>
+
+Picture::Picture(): head(nullptr), tail(nullptr){}
+
+//copy constructor
+Picture::Picture(const Picture& other): head(nullptr), tail(nullptr){
+    ListNode* current = other.head;
+    while (current != nullptr) {
+        add(*(current->shape)); // Assuming 'add' method clones the shape and adds it to the list
+        current = current->next;
+    }
+}
+
+//move constructor
+Picture::Picture(Picture&& other): head(nullptr), tail(nullptr){
+    other.head = nullptr;
+    other.tail = nullptr;
+}
+
+void Picture::swap(Picture& other){
+    std::swap(head, other.head);
+    std::swap(tail, other.tail);
+}
+
+Picture& Picture::operator=(const Picture& other){
+}
--- src/shape.hpp
+++ src/shape.hpp
@@ -2,3 +2,11 @@
 #include <string>
 
 // class Shape 
+Shape::Shape(Point center, std::string name) : center(center), name(name) {}
+
+void Shape::print(std::ostream& out) const {
+    out << name << "_(" << center.x << ", " << center.y << ")\n";
+}
+
+
+
