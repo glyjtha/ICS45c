38c6282d330258fe736769ca4060bf2579e087b8
Juntong Ye <juntoyt1@uci.edu>
2024-02-20 21:19:37 -0800

    finsihed draft

--- src/picture.cpp
+++ src/picture.cpp
@@ -33,3 +33,63 @@ Picture& Picture::operator=(const Picture& other){
     return *this;
 }
 
+Picture& Picture::operator=(Picture&& other){
+    if (this != &other) {
+        head = other.head;
+        tail = other.tail;
+
+        other.head = nullptr;
+        other.tail = nullptr;
+    }
+    return *this;
+}
+
+void Picture::add(const Shape& shape){
+     ListNode* newNode = new ListNode{shape.clone(), nullptr};
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+void Picture::print_all(std::ostream& out) const {
+    ListNode* current = head;
+    while (current != nullptr) {
+        current->shape->print(out);
+        current = current->next;
+    }
+}
+
+void Picture::draw_all(std::ostream& out) const{
+    ListNode* current = head;
+    while (current != nullptr){
+        current->shape ->draw(out);
+        current = current->next;
+    }
+}
+
+double Picture::total_area() const {
+    double area = 0.0;
+    ListNode* current = head;
+    while (current != nullptr) {
+        area += current->shape->area(); 
+        current = current->next;
+    }
+    return area;
+}
+
+Picture::~Picture() {
+    ListNode* current = head;
+    while (current != nullptr) {
+        ListNode* next = current->next;
+        delete current->shape; // Deallocate the shape
+        delete current; // Deallocate the node
+        current = next;
+    }
+    head = nullptr;
+    tail = nullptr;
+}
--- src/square.cpp
+++ src/square.cpp
@@ -1,1 +1,5 @@
 #include "square.hpp"
+
+Square::Square(Point center, std::string name, int side)
+    : Rectangle(center, name, side, side){}
+
